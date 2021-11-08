23b65ad0cfff72fa9bd301fa531be641005b9580
human264 <human264@gmail.com>
2021-11-08 19:19:45 +0900

    에러

---
+++ app/src/main/java/com/example/parayo/api/ApiGenerator.kt
@@ -0,0 +1,33 @@
+package com.example.parayo.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class ApiGenerator {
+    fun <T> generate(api: Class<T>): T = Retrofit.Builder()
+        .baseUrl(HOST)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient())
+        .build()
+        .create(api)
+
+    private fun httpClient() =
+        OkHttpClient.Builder().apply {
+            addInterceptor(httpLoggingInterceptor())
+        }.build()
+
+
+    private fun httpLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+
+    companion object {
+        const val HOST = "http://1.249.139.46:3308"
+    }
+
+
+}
--- app/src/main/java/com/example/parayo/intro/IntroActivity.kt
+++ app/src/main/java/com/example/parayo/intro/IntroActivity.kt
@@ -2,12 +2,27 @@ package com.example.parayo.intro
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parayo.api.ParayoApi
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.anko.setContentView
 
-class IntroActivity:Activity() {
+class IntroActivity: Activity() {
+    constructor() : super()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val ui = IntroActivityUI()
         ui.setContentView(this)
+
+        runBlocking {
+            try {
+                val response = ParayoApi.instance.hello()
+                response.data?.let { Log.d("IntroActivity", it) }
+            } catch (e: Exception) {
+                Log.e("IntroActivity", "HelloAPi 호출 오류", e)
+            }
+        }
     }
 }
